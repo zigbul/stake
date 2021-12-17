e252730e6adece0b7dfff49344e4b2763cbfc7fe
Zigbul <zigbul@gmail.com>
2021-12-17 23:33:57 +0300

    staked game

--- main.js
+++ main.js
@@ -2,31 +2,18 @@ const body = document.querySelector('body');
 const amount = document.querySelector('.amount');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const sum = document.querySelector('#sum');
 const table = document.querySelector('table');
+const tableRow = document.querySelector('.row');
 const select = document.querySelector('select');
+const D_KEYCODE = 68;
 
 function addCoin(e) {
-    if (e.keyCode === 68) {
+    if (e.keyCode === D_KEYCODE) {
         amount.innerText = +amount.innerText + 1;
     }
 }
 
-function addToStake() {
-    if (input.value === '') {
-        alert('Сначала введите кол-во монет для добавления в стейк');
-    } else if (+input.value > +amount.innerText) {
-        alert('Недостаточно монет');
-    } else {
-        alert(`В стейк добавлено ${input.value} монет`);
-        sum.innerText = +sum.innerText + +input.value;
-        input.value = '';
-    }
-}
-
 function addRow() {
-    const tableRow = document.createElement('tr');
-
     if (input.value === '') {
         alert('Сначала введите кол-во монет для добавления в стейк');
         return;
@@ -34,42 +21,45 @@ function addRow() {
     } else if (+input.value > +amount.innerText) {
         alert('Недостаточно монет');
         return;
-
+        
     } else {
-        // for (let i = 0; i < 4; i++) {
-        //     const tableData = document.createElement('td');
-        
-        //     if (i === 0) {
-        //             alert(`В стейк добавлено ${input.value} монет`);
-        //             tableData.innerText = input.value;
-        //             amount.innerText = +amount.innerText - +input.value;
-        //             input.value = '';
-        //     }
-        
-        //     if (i === 1) {
-        //         tableData.innerText = `${select.value}d`;
-        //     }
-        
-        //     if (i === 2) {
-        //         setInterval(() => {
-        //             tableData.innerText = `${+select.value - 1}d`;
-        //         }, 10)
-        //     }
-        
-        //     if (i === 3) {
-        //         tableData.innerText = 0;
-        //     }
-            
-        //     tableRow.append(tableData);
-        // }
-        tableRow.append(document.createElement('td').innerText = input.value);
+        let days = +select.value;
+        let interest;
+        let bonus = 0;
+        const stakedCoins = +input.value;
+
+        if (+select.value === 30) {
+            interest = 20;
+        } else if (+select.value === 60) {
+            interest = 35;
+        } else {
+            interest = 50;
+        }
+
+        const row = tableRow.cloneNode(true);
+
+        alert(`В стейк добавлено ${input.value} монет`);
+        row.children[0].innerHTML = input.value;
         amount.innerText = +amount.innerText - +input.value;
         input.value = '';
-        tableRow.append(document.createElement('td').innerText = `${select.value}d`);
-        tableRow.append(document.createElement('td').innerText = `${select.value}d`);
+        row.children[1].innerHTML = `${days}d`;
+        row.children[2].innerHTML = `${days}d`;
+        row.children[3].innerHTML = bonus;
+        table.append(row);
+
+        let timer = setInterval(update, 1000);
+        
+        function update() {
+            row.children[2].innerHTML = `${days--}d`;
+            bonus += +((Math.log10(stakedCoins) * interest).toFixed(4));
+            row.children[3].innerHTML = `${bonus}`;
+            if (days < 0) {
+                clearInterval(timer);
+                amount.innerText = (+amount.innerText + bonus).toFixed(4);
+                row.remove();
+            }
+        }
     }
-    
-    table.append(tableRow);
 }
 
 body.addEventListener('keydown', addCoin);
